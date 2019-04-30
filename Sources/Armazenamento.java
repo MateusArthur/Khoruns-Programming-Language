bf856e2269a26c4aa375b5a9f2a1122df8ad18cc
natani3l <natanielemes99@gmail.com>
2019-04-29 22:00:12 -0300

    Arrumado Armazenamento

--- Sources/Armazenamento.java
+++ Sources/Armazenamento.java
@@ -1,8 +1,19 @@
 class Armazenamento{
 	private Inteiro inteiros[];
-	
-	public Armazenamento(){
-		for(int x = 0; x < encontros.length;x ++)
-			this.inteiros[x] = new Inteiro();	
+
+	Armazenamento(){
+		this.inteiros = new Inteiro[20];
+
+		for(int i = 0; i < 20; i++){
+			inteiros[i] = new Inteiro();
+		}
 	}
-}	
+
+	public void setInteiro(Inteiro i){
+		this.inteiros[inteiros.length] = i;
+	}
+
+	public Inteiro getInteiro(int p){
+		return this.inteiros[p];
+	}
+}
