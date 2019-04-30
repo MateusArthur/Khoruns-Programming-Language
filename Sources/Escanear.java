c2ebdb7c81f07c2f141e0269ce54146a1acbc6c8
Mateus <mateusarthur00@homtail.com>
2019-04-29 21:05:20 -0300

    Inserções das variaveis incompleta

--- Sources/Escanear.java
+++ Sources/Escanear.java
@@ -6,18 +6,58 @@ class Escanear {
 		while (input.hasNext()) {
 			// Nome para a variavel inteiro
 			String line = input.nextLine();
-			line = line.replaceAll("\\s+", "");
+			line = line.replaceAll("\\s+", ""); // Remover espaços da linha
 			char [] letras = line.toCharArray();
-			if(letras[0] == 'n' && letras[1] == 'e' && letras[2] == 'w') {
-				int j = 3;
-				String x;
-				StringBuilder sb =  new StringBuilder();
-				while(letras[j] != '=') {
-					sb.append(letras[j]);
-					j++;
+			// Descobrir se é uma nova variavel
+			if(letras[0] == 'n' && letras[1] == 'e' && letras[2] == 'w') { 
+				boolean temTipo = false;
+				// Variavel Float
+				if(letras[4] == 'F' && letras[5] == 'l' && letras[6] == 'o' && letras[7] == 't' && letras[8] == ':') { 
+					temTipo = true;
 				}
-				x = sb.toString	();
-				System.out.println("Inteiro variavel: " + x);
+				// booleano
+				if(letras[4] == 'b' && letras[5] == 'o' && letras[6] == 'o' && letras[7] == 'l' && letras[8] == ':') { 
+					temTipo = true;
+				}
+				// não é definido após = 
+				if(temTipo == false) {
+					int j = 3;
+					// definir string/vetor
+					while(letras[j] != '[') { 
+						// armazenar tamanho do vetor
+						if(letras[j] == '[') {
+							temTipo = true;
+							int x = j;
+							StringBuilder sx =  new StringBuilder();
+							String tVet;
+							while(letras[x] != ']') // armazenar tamanho do vetor
+							{
+								if(Character.isDigit(letras[x]) == true) // verificar se é numero
+								{
+									System.out.println("[Khronus]: Erro de Syntaxe. [Linha %d]" + 0); // ajeitar
+									break;
+								}
+								sx.append(letras[x]);
+								x++;
+							}
+						}
+						j++;
+					}
+				}
+				int j = 3;
+				if(temTipo == false) { // se não houver tipo será inteiro ou definido após o =
+					String x;
+					StringBuilder sb =  new StringBuilder();
+					// dar loop para achar o =;
+					while(letras[j] != '=') { 
+						sb.append(letras[j]);
+						j++;
+						if(letras[j] == '=') // se tiver o =
+							temTipo = true;
+					}
+					x = sb.toString	();
+					System.out.println("Inteiro Variavel: " + x);
+				}
 				j++;
 				String xi;
 				StringBuilder sn = new StringBuilder();
