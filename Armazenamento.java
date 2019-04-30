59736e1c87459b677e175e115864f04200fce915
[Bruno Bevilaqua] <[b.bevilaqua69@gmail.com]>
2019-04-29 21:54:38 -0300

    Adicionado armazenamento para inteiros

---
+++ Armazenamento.java
@@ -0,0 +1,19 @@
+class Armazenamento{
+	private Inteiro inteiros[];
+	
+	Armazenamento(){
+		this.inteiros = new Inteiro[20];
+
+		for(int i = 0; i < 20; i++){
+			inteiros[i] = new Inteiro();
+		}
+	}
+
+	public void setInteiro(Inteiro i){
+		this.inteiros[inteiros.length] = i;
+	}
+
+	public Inteiro getInteiro(int p){
+		return this.inteiros[p];
+	}
+}	
