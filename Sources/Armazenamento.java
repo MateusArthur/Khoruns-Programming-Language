047d29ff12030d79c80fc0271b0a01fa8e0592da
[Bruno Bevilaqua] <[b.bevilaqua69@gmail.com]>
2019-05-03 13:54:47 -0300

    adicionado armazenamento para float

--- Sources/Armazenamento.java
+++ Sources/Armazenamento.java
@@ -1,9 +1,13 @@
 import java.util.*;
 
 class Armazenamento{
+	//Array para armazenar as variaveis tipo inteiro declaradas no arquivo .kh.
 	private List<Inteiro> inteiros = new ArrayList<Inteiro>();
 
+	//Array para armazenar as variaveis tipo float declaradas no arquivo .kh.
+	private List<Float> floats = new ArrayList<Float>();
 
+	//Cria um inteiro e armazena no armazenamento.
 	public void setInteiro(String n, int c){
 		Inteiro aux = new Inteiro();
 		
@@ -13,7 +17,17 @@ class Armazenamento{
 		inteiros.add(aux);
 	}
 
-	//Busca a variravel informada no parametro no armazenamento
+	//Cria um float e armazena no armazenamento.
+	public void setFloat(String n, int c){
+		Float aux = new Float();
+		
+		aux.setNome(n);
+		aux.setConteudo(c);
+
+		floats.add(aux);
+	}
+
+	//Busca a variravel informada no parametro no armazenamento.
 	public Inteiro getInteiro(String n){
 		for(int i = 0; i < this.inteiros.size(); i++){
 			
@@ -24,4 +38,17 @@ class Armazenamento{
 			
 		return null;
 	}	
+
+	//Busca a variravel informada no parametro no armazenamento.
+	public Float getFloat(String n){
+		for(int i = 0; i < this.floats.size(); i++){
+			
+			if(this.floats.get(i).getNome().equals(n)){
+				return this.floats.get(i);		
+			}
+		}		
+			
+		return null;
+	}	
+
 }
