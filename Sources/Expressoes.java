9738c5070c3fe30a7476d471e3fe95237635e434
natani3l <natanielemes99@gmail.com>
2019-05-16 17:45:56 -0300

    Classe p/ operações de (+ , -, /, *)

--- Sources/Expressoes.java
+++ Sources/Expressoes.java
@@ -1,21 +1,50 @@
 class Expressoes{
-  public void calcula(){
+  private int a, b;
 
+  public void calcula(String[] vetor, Inteiro var){
+    //a = vetor[0].charAt(0);
+    a = Integer.parseInt(vetor[0]);//Passar o valor do Indice [0] do Vetor de Character para Inteiro.
+    b = Integer.parseInt(vetor[2]);//Passar o valor do Indice [2] do Vetor de Character para Inteiro.
+
+    if(vetor[1].equals("+")){
+      var.setConteudo(this.soma(a, b));
+    }
+
+    else if(vetor[1].equals("-")){
+      var.setConteudo(this.subtrai(a, b));
+    }
+
+    else if(vetor[1].equals("*")){
+      var.setConteudo(this.multiplica(a, b));
+    }
+
+    else if(vetor[1].equals("/")){
+      var.setConteudo(this.divide(a, b));
+    }
+
+    else{
+      System.out.println("ERROR");
+    //  return null;
+    }
   }
 
-  public void soma(){
 
+  public int soma(int a, int b){
+      return a + b;
   }
 
-  public void subtrai(){
 
+  public int subtrai(int a, int b){
+      return a + b;
   }
 
-  public void divide(){
 
+  public int multiplica(int a, int b){
+      return a + b;
   }
 
-  public void multiplica(){
 
+  public int divide(int a, int b){
+      return a + b;
   }
 }
