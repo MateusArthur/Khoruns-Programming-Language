258c95989fb12b4d7d9265341c7dcf7fabd5fa51
Mateus Arthur <mateusarthur00@hotmail.com>
2019-05-16 15:13:37 -0300

    Deixado somente int

--- Sources/Atribuir.java
+++ Sources/Atribuir.java
@@ -1,31 +1,28 @@
 class Atribuir{
+
+	//Metodo para atribuir um valor inteiro a uma variavel.
+	public void atribuiVarInt(Inteiro a, int b){
+		a.setConteudo(b);
+	}
+
 	//Metodo para atribuir o valor de uma variavel a outra.
-	public void atribuiVarVar(Float a, Float b){
+	public void atribuiVarVar(Inteiro a, Inteiro b){
 		a.setConteudo(b.getConteudo() + a.getConteudo());
 	}
 
 	//Metodo para uso de var++
-	public void incrementaVar(Float a){
+	public void incrementaVar(Inteiro a){
 		a.setConteudo(a.getConteudo() + 1);
 	}
 
 	//Metodo para uso de var--
-	public void decrementaVar(Float a){
+	public void decrementaVar(Inteiro a){
 		a.setConteudo(a.getConteudo() - 1);
 	}
 
-	//Metodo para atribuir o valor de uma variavel a outra.
-	public void atribuiVarVar(Float a, Float b){
-		a.setConteudo(b.getConteudo() + a.getConteudo());
-	}
-
-//Metodo para atribuir um valor em uma variavel
-	public void atribuiVarVal(Float a, double val) {
-		a.setConteudo(a.getConteudo() + val);
-	}
-
 	//Para somar ou diminuir
-	/*public void addVar(Float a, float val) {
+	public void addVar(Inteiro a, int val) {
 		a.setConteudo(a.getConteudo() + val);
-	}*/
+	}
 }
+
