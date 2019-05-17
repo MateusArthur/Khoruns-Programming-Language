ce5d3ed1377c0d2b73084779b2a03b8a6e702b65
natani3l <natanielemes99@gmail.com>
2019-05-17 19:18:07 -0300

    Adiciona classe Print.java

---
+++ Sources/Print.java
@@ -0,0 +1,14 @@
+class Print{
+
+
+  public void printarNaTelaStringInteiro(String n, Inteiro i){
+    int aux = i.getConteudo();
+    System.out.println(n + i);
+  }
+
+  public void printarNaTelaString(String n){
+    System.out.println(n);
+  }
+
+
+}
