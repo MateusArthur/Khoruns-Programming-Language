9b40f498084e7fc042123438ead79c39a6763ec0
Mateus Arthur <mateusarthur00@hotmail.com>
2019-05-21 22:44:32 -0300

    Percorrer chaves If

---
+++ fonte/Print.java
@@ -0,0 +1,11 @@
+class Print{
+  public void printarNaTelaStringInteiro(String n, Inteiro i){
+    System.out.println(n + i.getConteudo());
+  }
+
+  public void printarNaTelaString(String n){
+    System.out.println(n);
+  }
+
+
+}
